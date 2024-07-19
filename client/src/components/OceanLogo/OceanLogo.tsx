f192d1ff0efbbc686c74131f23f7fe2808981766
OriEliyahu <ori.eliyahu@upwind.io>
2024-07-19 19:42:41 +0300

    fix: add user-select none

--- client/src/components/OceanLogo/OceanLogo.tsx
+++ client/src/components/OceanLogo/OceanLogo.tsx
@@ -6,7 +6,14 @@ export const OceanLogo: React.FC = () => {
   return (
     <div className="flex align-center column-gap-8">
       <ShoppingBagIcon color={colors.blue03} height="24px" width="24px" />
-      <Text text="Ocean" fontFamily="Courgette, cursive" fontSize={40} />
+      <Text
+        text="Ocean"
+        fontFamily="Courgette, cursive"
+        fontSize={40}
+        style={{
+          userSelect: "none",
+        }}
+      />
     </div>
   );
 };
