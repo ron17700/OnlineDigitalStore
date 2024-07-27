7780a69355f71bd5d2c2cfe392c58e1207d74328
moshikbaruch <moshikb@elementor.com>
2024-07-28 02:47:57 +0300

    chore: Add scraping route for data scraping

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 import productRoutes from './product.route';
 import cartRoutes from './cart.route';
+import scrapingRoutes from './scraping.route';
 const { isAuthorized } = require('../middlewares/auth');
 
 router.use('/product', isAuthorized, productRoutes);
 router.use('/cart', isAuthorized, cartRoutes);
+router.use('/scraping', isAuthorized, scrapingRoutes);
 
 export default router
