26919678c989301be88b5930b5e56c5c9f803266
Ron Yaari <ryaari@salesforce.com>
2024-08-02 13:34:24 +0300

    Fix sanatize

--- server/index.ts
+++ server/index.ts
@@ -18,7 +18,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(mongoSanitize);
+app.use(mongoSanitize());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.resolve(__dirname, './public')));
 
