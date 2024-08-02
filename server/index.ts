5e915b876371b3580f5b0ac9cf5de71eb29210ef
OriEliyahuCs <ori951753@cs.colman.ac.il>
2024-08-02 18:16:41 +0300

    fix: use-imports-instead-of-require (#14)
    Co-authored-by: OriEliyahu <ori.eliyahu@upwind.io>

--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,15 @@
-import path from 'path';
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import path from "path";
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoSanitize from "express-mongo-sanitize";
 
-import mainRoutes from './routes/index';
-import errorHandler from './middlewares/errorHandler';
-const { isAuthorized} = require('./middlewares/auth');
-const cors = require('cors');
-const mongoSanitize = require('express-mongo-sanitize');
+import mainRoutes from "./routes/index";
+import errorHandler from "./middlewares/errorHandler";
+import { isAuthorized } from "./middlewares/auth";
 
-dotenv.config({ path: path.join(__dirname, './.env') });
+dotenv.config({ path: path.join(__dirname, "./.env") });
 
 process.env.rootDir = __dirname;
 
@@ -20,25 +20,25 @@ app.use(express.json());
 app.use(cors());
 app.use(mongoSanitize());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.resolve(__dirname, './public')));
+app.use(express.static(path.resolve(__dirname, "./public")));
 
-app.use('/', isAuthorized,  mainRoutes);
+app.use("/", isAuthorized, mainRoutes);
 
 app.use(errorHandler);
 
 const start = async () => {
-    try {
-        console.log('Trying to connect to MongoDB...\n');
-        await mongoose.connect(process.env.MONGO_URI as string);
-        console.log('MongoDB connected successfully\n');
-    } catch (error) {
-        console.error((error as Error).message);
-        console.log((error as Error).stack);
-    }
+  try {
+    console.log("Trying to connect to MongoDB...\n");
+    await mongoose.connect(process.env.MONGO_URI as string);
+    console.log("MongoDB connected successfully\n");
+  } catch (error) {
+    console.error((error as Error).message);
+    console.log((error as Error).stack);
+  }
 
-    app.listen(PORT, () => {
-        console.log(`Server is listening on port: ${PORT}\n`);
-    });
+  app.listen(PORT, () => {
+    console.log(`Server is listening on port: ${PORT}\n`);
+  });
 };
 
 start();
