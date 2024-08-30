72df9c98b59d73bc7227bfe532c8f4b4bd05d1f7
moshikbaruch <moshikb@elementor.com>
2024-08-30 14:00:45 +0300

    adding dokcer compose for server and client

--- server/scraper/index.ts
+++ server/scraper/index.ts
@@ -6,7 +6,7 @@ import CategoryService from '../services/category.service';
 
 const axiosInstance = axios.create({
     headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3029.110 Safari/537.3'
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36'
     }
 });
 
